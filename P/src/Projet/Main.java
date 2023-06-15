b09a265300b36f44ca7d4359ddb6951ba870ccaa
Class01 <Class01@DESKTOP-46EHI55>
2023-06-15 21:10:51 +0900

    2023-06-15

---
+++ P/src/Projet/Main.java
@@ -0,0 +1,138 @@
+package Projet;
+
+import java.awt.Button;
+import java.awt.Label;
+import java.awt.TextField;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+
+public class Main extends WindowAdapter implements ActionListener {
+	private JFrame f, fMain;
+	private TextField tfId, tfPwd, tfMsg;
+	private Button bLogin;
+	private MemberDAO dao;
+	
+
+	public Main() {
+		dao = new MemberDAO();
+
+		f = new JFrame("Login Frame");
+		f.setSize(500, 300);
+		f.setLocation(500, 10); // 따라 하지 마세요!
+		f.setLayout(null);
+		f.addWindowListener(this);
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//CLOSE
+		
+		
+		//이미지 넣기-----------------------------
+		try {
+			f.setContentPane(new JLabel(new ImageIcon(ImageIO.read(new File("C:\\Users\\Class01\\Desktop\\main.jpg")))));
+			}catch(IOException e) {
+				e.printStackTrace();
+			}
+		f.pack();
+		//이미지 넣기 완----------------------
+		
+		
+	
+
+		JLabel lid = new JLabel("ID : ");
+		lid.setBounds(50, 60, 100, 40);
+		
+		
+		
+		
+
+		tfId = new TextField();
+		tfId.setBounds(160, 60, 190, 40);
+
+		Label lpwd = new Label("Password : ");
+		lpwd.setBounds(50, 130, 100, 40);
+
+		tfPwd = new TextField();
+		tfPwd.setBounds(160, 130, 190, 40);
+		tfPwd.setEchoChar('*');
+
+		bLogin = new Button("Login");
+		bLogin.setBounds(380, 90, 50, 50);
+		bLogin.addActionListener(this);
+
+		tfMsg = new TextField();
+		tfMsg.setBounds(50, 180, 370, 40);
+		tfMsg.setEditable(false);
+		tfMsg.setFocusable(false);
+		
+	
+		
+		
+		
+		
+		
+		
+
+		f.add(lid);
+		f.add(tfId);
+		f.add(lpwd);
+		f.add(tfPwd);
+		f.add(bLogin);
+		f.add(tfMsg);
+		
+
+		f.setVisible(true);
+		
+		
+		
+		
+		
+	}
+
+	public static void main(String[] args) {
+		new Main();
+	}
+
+
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		System.out.println("Click!");
+		System.out.println(tfId.getText() + " : " + tfPwd.getText());
+
+		String strId = tfId.getText();
+
+		ArrayList<MemberVo> list = dao.list(strId);
+
+		System.out.println("list.size() : " + list.size());
+		if (list.size() == 1) {
+			MemberVo data = (MemberVo) list.get(0);
+			String id = data.getId();
+			String pwd = data.getPassword();
+
+			System.out.println("DB ==> " + id + " : " + pwd);
+
+			if (tfPwd.getText().equals(pwd)) {
+				System.out.println("로그인이 되었습니다!");
+				tfMsg.setText("로그인이 되었습니다!");
+
+				fMain = new JFrame("메인프레임");
+				fMain.setBounds(200, 200, 200, 200);
+				fMain.addWindowListener(this);
+				fMain.setVisible(true);
+			} else {
+				System.out.println("다시 입력하세요.");
+				tfMsg.setText("다시 입력하세요.");
+			}
+		} else {
+			tfMsg.setText("ID가 틀렸습니다. 다시 입력하세요.");
+		}
+	}
+}
